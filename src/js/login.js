664788573f908a559b15cd7ddf2832f5ec345e26
Fede Ubeda <federicobeda@gmail.com>
2025-11-01 23:04:36 -0300

    Desplegables Mi cuenta y Carrito

--- src/js/login.js
+++ src/js/login.js
@@ -11,8 +11,9 @@ $(document).ready(function(){
             );
 
             if (usuarioEncontrado) {
-                alert("✅ Inicio de sesión correcto. Bienvenido " + usuarioEncontrado.user);
+                alert("✅ Inicio de sesión correcto. Bienvenido " + usuarioEncontrado.user + ".");
                 sessionStorage.setItem("usuarioLogueado", "1");
+                sessionStorage.setItem("usuarioNombre", usuarioEncontrado.user);
                 window.location.href = "../pages/index.html"
                 //redirigir
             } else {
@@ -22,21 +23,59 @@ $(document).ready(function(){
             alert("Error al cargar el archivo JSON");
         });
     });
+
+
+    const logueado = sessionStorage.getItem("usuarioLogueado")
+    if (logueado == 1){
+        $(".login").hide()
+        $(".logout").show()
+        console.log(logueado)
+    } else {
+        $(".logout").hide()
+        $(".miCuenta").hide()
+        $(".login").show()
+    }
+
+    /*MI CUENTA*/
+    $(".logout").click(function(e) {
+        e.preventDefault();
+        $(".miCuenta").toggle();
+
+        const nombre = sessionStorage.getItem("usuarioNombre");
+        $(".miCuenta").html(
+            `<p style="margin:15px;">¡Bienvenido, <strong>${nombre}</strong>!</p>
+            <a class="salir" href="../pages/index.html"><strong>SALIR</strong></a>`
+        );
+    });
+
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.user-menu').length) {
+            $(".miCuenta").hide();
+        }
+    });
+
+    /*SALIR*/
+    $(".salir").click(function() {
+        sessionStorage.setItem("usuarioLogueado", "0");
+        alert("Usted ha salido de su cuenta exitosamente.");
+    });
+
+
+    /*CARRITO*/
+    $(".carrito-desplegable").click(function(e) {
+        e.preventDefault();
+        $(".carrito-cont").toggle();
+
+        $(".carrito-cont").html(
+            `<p style="margin:15px;"><strong>MIS COMPRAS</strong></p>
+            <a class="salir" href="../pages/index.html"><strong>SALIR</strong></a>`
+        );
+    });
+
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.carrito-menu').length) {
+            $(".carrito-cont").hide();
+        }
+    });
+
 });
-
-const logueado = sessionStorage.getItem("usuarioLogueado")
-
-if (logueado == 1){
-    $(".login").hide()
-    $(".logout").show()
-    console.log(logueado)
-
-} else {
-    $(".logout").hide()
-    $(".login").show()
-}
-
-console.log(logueado)
-
-
-
